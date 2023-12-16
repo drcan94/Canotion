cca60a5def46e31b9efab1c849c93efd646d5c8a
Burak Can <drcan94@gmail.com>
2023-12-16 03:56:09 +0300

    preview fixed

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -14,15 +14,16 @@ interface DocumentIdPageProps {
   params: {
     documentId: Id<"documents">;
   };
-};
+}
 
-const DocumentIdPage = ({
-  params
-}: DocumentIdPageProps) => {
-  const Editor = useMemo(() => dynamic(() => import("@/components/editor"), { ssr: false }) ,[]);
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    []
+  );
 
   const document = useQuery(api.documents.getById, {
-    documentId: params.documentId
+    documentId: params.documentId,
   });
 
   const update = useMutation(api.documents.update);
@@ -30,7 +31,7 @@ const DocumentIdPage = ({
   const onChange = (content: string) => {
     update({
       id: params.documentId,
-      content
+      content,
     });
   };
 
@@ -51,11 +52,11 @@ const DocumentIdPage = ({
   }
 
   if (document === null) {
-    return <div>Not found</div>
+    return <div>Not found</div>;
   }
 
-  return ( 
-    <div className="pb-40">
+  return (
+    <div className="pb-40 dark:bg-[#1F1F1F]">
       <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar preview initialData={document} />
@@ -67,6 +68,6 @@ const DocumentIdPage = ({
       </div>
     </div>
   );
-}
- 
+};
+
 export default DocumentIdPage;
