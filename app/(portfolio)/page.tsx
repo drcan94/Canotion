bc7939f7a1709d5ee07fe7940267a2b1953cf1e4
Burak CAN <drcan94@gmail.com>
2024-10-12 15:55:23 +0300

    updates

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -8,9 +8,17 @@ import {
 } from "@/components/ui/accordion";
 import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
-import { useState } from "react";
+import React, { useState } from "react";
+import ReactMarkdown, { Components } from "react-markdown";
 
-// Step 1: Setting up the main layout (App Directory)
+const components: Components = {
+  a: (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => (
+    <a {...props} className="underline text-blue-500 hover:text-blue-700">
+      {props.children}
+    </a>
+  ),
+};
+
 export default function Dashboard() {
   const [language, setLanguage] = useState("tr");
 
@@ -49,36 +57,32 @@ export default function Dashboard() {
       subHeader: "An overview of my work and experiences",
       resume: {
         title: "Resume",
-        content:
-          "I am an anesthesiology resident and developer. Born in 1994, I started my medical studies at Atatürk University in 2013 and transferred to Sakarya University in 2017, graduating in January 2023. During the COVID-19 pandemic (around April 2020), I decided to shift my focus from studying for the TUS (Medical Specialty Exam) to software development. Within my first 6 months, I built my first amateur website—a magazine system. The code can be found here: GitHub - Journalist. I met my wife, Zehra, in December 2015, and we married in September 2020. We have a son who was born in February 2022.",
+        content: `I am an anesthesiology resident and developer. Born in 1994, I started my medical studies at Atatürk University in 2013 and transferred to Sakarya University in 2017, graduating in January 2023. During the COVID-19 pandemic (around April 2020), I decided to shift my focus from studying for the TUS (Medical Specialty Exam) to software development. Within my first six months, I built my first amateur website: [Journal System](https://github.com/drcan94/journalist). I met my wife, Zehra, in December 2015, and we married in September 2020. We have a son who was born in February 2022.`,
       },
       academicCareer: {
         title: "Academic Career",
-        content:
-          "After an extended academic journey, I completed my medical degree and worked as a general practitioner for 1.5 years in various hospitals in Konya (Taşkent, Hadim, Bozkır, Beyhekim) and in Hatay/Samandağ. During this time, I also completed my mandatory government service. In March 2024, I took the TUS exam and got accepted into the Necmettin Erbakan University Faculty of Medicine (Meram) Anesthesia program. I started this journey in September 2024 and am currently at the beginning of my specialty training, learning under the supervision of experienced mentors and senior colleagues.",
+        content: `After an extended academic journey, I completed my medical degree and worked as a general practitioner for 1.5 years in various hospitals in Konya (Taşkent, Hadim, Bozkır, Beyhekim) and in Hatay/Samandağ. During this time, I also completed my mandatory government service. In March 2024, I took the TUS exam and was accepted into the Anesthesiology program at Necmettin Erbakan University Faculty of Medicine (Meram). I started this journey in September 2024 and am currently at the beginning of my specialty training, learning under the supervision of experienced mentors and senior colleagues.`,
       },
       hobbies: {
         title: "Hobbies",
-        content:
-          "I love playing chess, coding for hours, and exploring new technologies. Especially while working in my field, creating software solutions to make my tasks easier—or imagining potential solutions, even if they don't always materialize—is very exciting.",
+        content: `I love playing chess, coding for hours, and exploring new technologies. Especially while working in my field, creating software solutions to make my tasks easier—or imagining potential solutions, even if they don't always materialize—is very exciting.`,
       },
       projects: {
         title: "Projects",
         content: [
-          "Intraoperative Fluid Tracker: A system built with Next.js and Tailwind CSS to automate fluid calculations for patients during surgery.",
-          "Educational Content Platform: Created educational content with Notion, accessible via Shopier, aimed at new doctors.",
+          `[Intraoperative Fluid Tracker](https://github.com/drcan94/intraoperative-fluid-tracker): A system built with Next.js and Tailwind CSS to automate fluid calculations for patients during surgery.`,
+          `[Educational Content Platform](https://shopier.com/your-link): Created educational content with Notion, accessible via Shopier, aimed at new doctors.`,
         ],
       },
       familyInfo: {
         title: "Family Information",
-        content:
-          "I met my wife, Zehra, in December 2015, and we married in September 2020. In February 2022, we welcomed our lovely son :)",
+        content: `I met my wife, Zehra, in December 2015, and we married in September 2020. In February 2022, we welcomed our lovely son. 😊`,
       },
       languages: {
         title: "Languages Known",
         content: [
-          "Programming Languages: Python (Django-DRF), JavaScript (Next.js, TypeScript)",
-          "Human Languages: English (intermediate), Turkish (native)",
+          `Programming Languages: Python (Django REST Framework), JavaScript (Next.js, TypeScript)`,
+          `Human Languages: English (intermediate), Turkish (native)`,
         ],
       },
     },
@@ -87,36 +91,32 @@ export default function Dashboard() {
       subHeader: "Çalışmalarım ve deneyimlerimin bir özeti",
       resume: {
         title: "Özgeçmiş",
-        content:
-          "Anestezi asistanı ve geliştiriciyim. 1994 doğumluyum, 2013 yılında Atatürk Üniversitesi'nde tıp fakültesine başladım ve 2017'de Sakarya Üniversitesi'ne yatay geçiş yaptım, Ocak 2023'te mezun oldum. COVID-19 pandemisi sırasında (2020 Nisan civarı), TUS'a (Tıpta Uzmanlık Sınavı) çalışmayı bırakıp yazılıma yönelmeye karar verdim. İlk 6 ayımda, ilk amatör web sitemi—bir dergi sistemi—yaptım. Kodları burada bulabilirsiniz: GitHub - Journalist. Eşim Zehra ile Aralık 2015'te tanıştım ve Eylül 2020'de evlendik. Şubat 2022'de doğan bir oğlumuz var.",
+        content: `Anestezi asistanı ve geliştiriciyim. 1994 doğumluyum. 2013 yılında Atatürk Üniversitesi Tıp Fakültesi'ne başladım ve 2017'de Sakarya Üniversitesi'ne yatay geçiş yaptım, Ocak 2023'te mezun oldum. COVID-19 pandemisi sırasında (2020 Nisan civarı), TUS'a (Tıpta Uzmanlık Sınavı) çalışmayı bırakıp yazılıma yönelmeye karar verdim. İlk altı ayımda ilk amatör web sitem olan bir [dergi sistemini](https://github.com/drcan94/journalist) yaptım. Eşim Zehra ile Aralık 2015'te tanıştım ve Eylül 2020'de evlendik. Şubat 2022'de bir oğlumuz oldu.`,
       },
       academicCareer: {
         title: "Akademik Kariyer",
-        content:
-          "Uzatmalı akademik yolculuğumun ardından tıp eğitimimi tamamladım ve 1.5 yıl boyunca Konya'nın çeşitli hastanelerinde (Taşkent, Hadim, Bozkır, Beyhekim) ve Hatay Samandağ'da pratisyen hekim olarak çalıştım. Bu süre zarfında devlet hizmet yükümlülüğümü de tamamladım. Mart 2024'te TUS'a girdim ve Necmettin Erbakan Üniversitesi Tıp Fakültesi (Meram) Anestezi bölümünü kazandım. Eylül 2024'te başladığım bu yolculuğun henüz başındayım. Akredite bir klinikte, yetkin hocalarımızın ve kıdemlilerimizin gözetiminde uzmanlık eğitimi sürecim devam ediyor.",
+        content: `Uzatmalı akademik yolculuğumun ardından tıp eğitimimi tamamladım ve 1.5 yıl boyunca Konya'nın çeşitli hastanelerinde (Taşkent, Hadim, Bozkır, Beyhekim) ve Hatay/Samandağ'da pratisyen hekim olarak çalıştım. Bu süre zarfında devlet hizmet yükümlülüğümü de tamamladım. Mart 2024'te TUS'a girdim ve Necmettin Erbakan Üniversitesi Meram Tıp Fakültesi Anesteziyoloji ve Reanimasyon bölümünü kazandım. Eylül 2024'te başladığım bu yolculuğun henüz başındayım. Akredite bir klinikte, yetkin hocalarımızın ve kıdemlilerimizin gözetiminde uzmanlık eğitimi sürecim devam ediyor.`,
       },
       hobbies: {
         title: "Hobiler",
-        content:
-          "Satranç oynamayı, saatlerce kod yazmayı ve yeni teknolojileri keşfetmeyi seviyorum. Özellikle mesleğimi yaparken yazılımla işlerimi kolaylaştırmak ve bazen gerçekleşmese de çözümler hayal etmek çok heyecan verici",
+        content: `Satranç oynamayı, saatlerce kod yazmayı ve yeni teknolojileri keşfetmeyi seviyorum. Özellikle mesleğimi yaparken yazılımla işlerimi kolaylaştırmak veya bazen gerçekleşmese de çözümler hayal etmek çok heyecan verici.`,
       },
       projects: {
         title: "Projeler",
         content: [
-          "İntraoperatif Sıvı Takipçisi: Ameliyat sırasında hasta sıvı hesaplamalarını otomatikleştirmek için Next.js ve Tailwind CSS kullanılarak oluşturulmuş bir sistem.",
-          "Eğitim İçeriği Platformu: Yeni doktorlar için Shopier üzerinden erişilebilen Notion ile oluşturulmuş eğitim içerikleri.",
+          `[İntraoperatif Sıvı Takipçisi](/sivi): Ameliyat sırasında hasta sıvı hesaplamalarını otomatikleştirmek için Next.js ve Tailwind CSS kullanılarak oluşturulmuş bir sistem.`,
+          `[Eğitim İçeriği Platformu](https://www.shopier.com/28216295): Yeni doktorlar için Shopier üzerinden erişilebilen, Notion ile oluşturulmuş eğitim içerikleri.`,
         ],
       },
       familyInfo: {
         title: "Aile Bilgileri",
-        content:
-          "Sevgili eşim Zehra ile Aralık 2015'te tanıştım ve Eylül 2020'de evlendik. Şubat 2022'de dünya tatlısı bir oğlumuz oldu :)",
+        content: `Eşim Zehra ile Aralık 2015'te tanıştım ve Eylül 2020'de evlendik. Şubat 2022'de dünya tatlısı bir oğlumuz oldu. 😊`,
       },
       languages: {
         title: "Bilinen Diller",
         content: [
-          "Yazılım Dilleri: Python (Django-DRF), JavaScript (Next.js, TypeScript)",
-          "İnsan Dilleri: İngilizce (orta), Türkçe (ana dil)",
+          `Yazılım Dilleri: Python (Django REST Framework), JavaScript (Next.js, TypeScript)`,
+          `İnsan Dilleri: İngilizce (orta), Türkçe (ana dil)`,
         ],
       },
     },
@@ -158,7 +158,9 @@ export default function Dashboard() {
               {content[language].resume.title}
             </AccordionTrigger>
             <AccordionContent className="text-base text-gray-700 dark:text-gray-300">
-              {content[language].resume.content}
+              <ReactMarkdown components={components}>
+                {content[language].resume.content}
+              </ReactMarkdown>
             </AccordionContent>
           </AccordionItem>
 
@@ -185,9 +187,13 @@ export default function Dashboard() {
               {content[language].projects.title}
             </AccordionTrigger>
             <AccordionContent className="text-base text-gray-700 dark:text-gray-300">
-              <ul className="list-disc list-inside">
+              <ul className="list-disc list-inside [&>li>p]:inline [&>li>p]:m-0">
                 {content[language].projects.content.map((project, index) => (
-                  <li key={index}>{project}</li>
+                  <li key={index}>
+                    <ReactMarkdown components={components}>
+                      {project}
+                    </ReactMarkdown>
+                  </li>
                 ))}
               </ul>
             </AccordionContent>
@@ -207,7 +213,7 @@ export default function Dashboard() {
               {content[language].languages.title}
             </AccordionTrigger>
             <AccordionContent className="text-base text-gray-700 dark:text-gray-300">
-              <ul className="list-disc list-inside">
+              <ul className="list-disc list-inside list-ma">
                 {content[language].languages.content.map(
                   (languageItem, index) => (
                     <li key={index}>{languageItem}</li>
